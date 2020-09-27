d3b84deb15f3c2ed270499b037505e70f64caa49
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-27 17:54:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,9 +27,9 @@ end
 def return_string_array(array)
   count = 0
   while count < array.length do
-    new_array.push(array[count].to_s)
+    array[count] = array[count].to_s
     count += 1
   end
-  new_array
+  array
 end
   
